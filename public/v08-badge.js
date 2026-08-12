47bf19f5d871b618c2ef3069f553df37359f1b2c
tomtaml <tom.tamlander@gmail.com>
2026-08-12 15:55:51 +0300

    Add v0.8 dispatcher preview badge

---
+++ public/v08-badge.js
@@ -0,0 +1,11 @@
+const brand=document.querySelector('.brand-block');
+if(brand&&!document.querySelector('#v08BuildBadge')){
+  document.querySelector('#v071BuildBadge')?.remove();
+  document.querySelector('#v07BuildBadge')?.remove();
+  document.querySelector('#v06BuildBadge')?.remove();
+  const badge=document.createElement('span');
+  badge.id='v08BuildBadge';
+  badge.className='status-badge';
+  badge.textContent='UI v0.8 dispatcher preview';
+  brand.appendChild(badge);
+}
