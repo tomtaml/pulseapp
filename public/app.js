27a5864a8a834e238e6113c6cc89c0b05bbe4428
tomtaml <tom.tamlander@gmail.com>
2026-08-13 17:21:53 +0300

    Load v0.9.1 fleet manager refinements

--- public/app.js
+++ public/app.js
@@ -11,6 +11,7 @@ import "./v08-dispatcher-cleanup.js";
 import "./v081-style.js";
 import "./v081-dispatcher.js";
 import "./v09-fleet-manager-fixed.js";
+import "./v091-fleet-manager.js";
 import "./v06-version.js";
 import "./v06-badge.js";
 import "./v07-badge.js";
