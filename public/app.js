9fb5c1fe1d690fa7a8f9d97c6f75661f11a8be60
tomtaml <tom.tamlander@gmail.com>
2026-08-25 16:59:19 +0300

    Load workshop cycle fixes with fresh module versions

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 import "./js/research-test-browser-shim.js";
 import "./js/main.js";
 import "./js/augment.js";
-import "./js/charging/index.js";
+import "./js/charging/index.js?v=1402";
 import "./v05.js";
 import "./v06.js";
 import "./v06-positioning-fix.js";
@@ -32,4 +32,4 @@ import "./v132-participant-bridge.js";
 import "./v134-battery-consistency.js";
 import "./v135-v2g-transition-fix.js";
 import "./v136-mobility-hold.js?v=1311";
-import "./v140-workshop-cycle-dedupe.js?v=1401";
+import "./v140-workshop-cycle-dedupe.js?v=1402";
